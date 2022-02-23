cfe750ee3afcc2f281400ae0e713928bc3c4a092
dhammena <dhammena@outlook.com>
2022-02-23 15:37:56 +0800

    add cover

--- ncped/index.ts
+++ ncped/index.ts
@@ -1,18 +1,20 @@
 import fs from "fs";
 import download from "download";
+import { render } from "template-file";
 
 interface IConfig {
   jsonUrl: string;
   jsonFile: string;
   txtFile: string;
-  cssFile: string;
+  templateFile: string;
+  cssFileName: string;
 }
 
 interface IWord {
   entry: string;
   grammar?: string;
   definition?: string | string[];
-  xr?: string | string[];
+  xr?: string | string[]; // cross references
 }
 
 class Ncpde {
@@ -28,55 +30,50 @@ class Ncpde {
 
   private generateWordHtml(word: IWord): string {
     //
-    let definitionItemHtml: string = "";
-    if (word.definition) {
-      if (typeof word.definition === "string") {
-        definitionItemHtml = `
-  <li>${word.definition}</li>`;
-      } else {
-        for (let item of word.definition) {
-          definitionItemHtml += `
-  <li>${item}</li>`;
-        }
-      }
-    }
-    if (definitionItemHtml) {
-      definitionItemHtml = `<ul class="definition">${definitionItemHtml}
-</ul>`;
+    let grammarHtml: string = ""
+    if (word.grammar) {
+      grammarHtml = `<span class="grammar">( ${word.grammar} )</span>`;
     }
 
     //
-    let crossRefHtml: string = "";
-    if (word?.xr) {
-      if (typeof word.xr === "string") {
-        crossRefHtml = `
-  <li>
-    <a href="entry://${word.xr}">${word.xr}</a>
-  </li>`;
+    let definitionListHtml: string = "";
+    if (word.definition) {
+      if (typeof word.definition === "string") {
+        definitionListHtml = `\n  <li>${word.definition}</li>`;
       } else {
-        for (let item of word.xr) {
-          crossRefHtml += `
-  <li>
-    <a href="entry://${item}">${item}</a>
-  </li>`;
+        for (let item of word.definition) {
+          definitionListHtml += `\n  <li>${item}</li>`;
         }
       }
     }
-    if (crossRefHtml) {
-      crossRefHtml = `<ul class="crossRef">${crossRefHtml}
-</ul>`;
+    if (definitionListHtml) {
+      definitionListHtml = `<ul class="definition">${definitionListHtml}\n</ul>`;
     }
 
-    return `
-${word.entry}
-<link rel="stylesheet" type="text/css" href="${this.config.cssFile}" />
-<div class="title">
-  <span class="entry">${word.entry}</span>
-  <span class="grammar">( ${word.grammar ?? ""} )</span>
-</div>
-${definitionItemHtml}
-${crossRefHtml}
-</>`;
+    //
+    let xfListHtml: string = "";
+    if (word?.xr) {
+      if (typeof word.xr === "string") {
+        xfListHtml = `\n  <li><a href="entry://${word.xr}">${word.xr}</a></li>`;
+      } else {
+        for (let item of word.xr) {
+          xfListHtml += `\n  <li><a href="entry://${item}">${item}</a></li>`;
+        }
+      }
+    }
+    if (xfListHtml) {
+      xfListHtml = `<ul class="crossRef">${xfListHtml}\n</ul>`;
+    }
+    
+    const data = {
+      entry: word.entry,
+      grammarHtml: grammarHtml,
+      cssFileName: this.config.cssFileName,
+      definitionListHtml: definitionListHtml,
+      xfListHtml: xfListHtml,
+    };
+    const templateString = fs.readFileSync(this.config.templateFile, "utf8");
+    return render(templateString, data);
   }
 
   private generateTxtFile() {
@@ -87,7 +84,8 @@ ${crossRefHtml}
       word = <IWord>word;
       resultString += this.generateWordHtml(word);
     }
-    resultString = resultString.trimStart().replace(/\n/g, "\r\n");
+    // Replace LF with CRLF
+    // resultString = resultString.replace(/\n/g, "\r\n");
     fs.writeFileSync(this.config.txtFile, resultString, "utf-8");
   }
 
@@ -103,7 +101,8 @@ export async function generateMdx() {
       "https://raw.githubusercontent.com/suttacentral/sc-data/master/dictionaries/simple/en/pli2en_ncped.json",
     jsonFile: `${__dirname}/ncpde.json`,
     txtFile: `${__dirname}/ncpde.txt`,
-    cssFile: "ncpde.css",
+    templateFile: `${__dirname}/template.html`,
+    cssFileName: "ncpde.css"
   };
   let ncpde = new Ncpde(config);
   ncpde.generate();
